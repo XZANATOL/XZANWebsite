0b0ab031ea4694408f479447144eb5477bb6a9fe
XZANATOL <abdelrahmanbedox@gmail.com>
2022-08-29 14:58:38 +0200

    Added Redirects '/r/' files

---
+++ routes/redirects.js
@@ -0,0 +1,16 @@
+module.exports = function(server, db){
+
+	server.get("/r/", async (request, response) => {
+			return response.render("404.ejs")
+	})
+
+	server.get("/r/:link", async (request, response) => {
+		const link = await db.collection("redirects").get(request.params.link)
+
+		if(!link){
+			return response.render("404.ejs")
+		}
+		return response.redirect(link.props.link)
+	})
+
+}
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
+//dev use
+require('dotenv').config()
+const port = 3000
+
 const express = require("express")
 const server = express()
+
+const CyclicDb = require("cyclic-dynamodb")
+const db = CyclicDb(process.env.AWS_TABLE_NAME)
 
 server.set('view engine', 'ejs')
 server.set("views", "./views/")
 
 server.use(express.static("public"))
 
-let port = 3000
-
 require("./routes/home")(server)
+require("./routes/redirects")(server, db)
 require("./routes/portfolio")(server)
-
-
 
 server.listen(port, () => {
 	console.log(`Listening on port ${port}`)
