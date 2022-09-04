acc1457bb012335cc4a0a8809ea14311b8464e05
XZANATOL <abdelrahmanbedox@gmail.com>
2022-09-04 17:31:03 +0200

    Added Portfolio '/portfolio' workExperience popup

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,3 +1,8 @@
-async function copy_to_clip(text) {
+async function copy_to_clip(text){
     await navigator.clipboard.writeText(text)
+}
+
+function switch_popup(id){
+    let popup = document.getElementById(id)
+    popup.classList.toggle("show")
 }
