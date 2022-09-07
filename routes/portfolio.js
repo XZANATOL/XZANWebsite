951f63b0eb3832d8f38d916fec203a8bea84f0f4
XZANATOL <abdelrahmanbedox@gmail.com>
2022-09-07 18:36:02 +0200

    Completed Portfolio '/portfolio' resume section

--- routes/portfolio.js
+++ routes/portfolio.js
@@ -7,7 +7,10 @@ module.exports = function(server, db){
 			"greetings": data.props.greetings,
 			"discord_handle": data.props.discord_handle,
 			"resume": data.props.resume,
-			"workExperience": data.props.workExperience
+			"workExperience": data.props.workExperience,
+			"educationExperience": data.props.educationExperience,
+			"volunteer": data.props.volunteer,
+			"techStack": data.props.techStack
 		}
 		return response.render("portfolio/index.ejs", context)
 	})
