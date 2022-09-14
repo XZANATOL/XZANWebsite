da5e362548e5435509480ab71a636cc6917a234e
XZANATOL <abdelrahmanbedox@gmail.com>
2022-09-14 16:10:27 +0200

    added Blog '/blog/:cat' files

--- routes/blog.js
+++ routes/blog.js
@@ -1,10 +1,28 @@
+const fs = require("fs")
+
 module.exports = function(server){
 
 	server.get("/blog", (request, response) => {
+		const dir = fs.readdirSync(`${__dirname}/../views/blog-articles/`)
 		const context = {
-			"postCount": 5
+			"postCount": 5,
+			"categories": dir
 		}
+		return response.render("blog/index.ejs", context)
+	})
 
+	server.get("/blog/:cat", (request, response) => {
+		let dir;
+		try{
+			dir = fs.readdirSync(`${__dirname}/../views/blog-articles/${request.params.cat}`)
+		} catch{
+			dir = []
+		}
+		const context = {
+			"postCount": 5,
+			"categories": dir
+		}
+		// View will be changed with the next commit
 		return response.render("blog/index.ejs", context)
 	})
 
