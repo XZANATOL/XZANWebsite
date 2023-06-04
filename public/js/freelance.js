93b73c9c39c84386354d845a406c62addd84cf8e
XZANATOL <abdelrahmanbedox@gmail.com>
2023-06-04 15:38:44 +0300

    Adjusted Some CSS
    * Fixed visual height error in homepage.
    * Fixed image width setting in '.m3u8 Streaming' blog.
    * Fixed text overflow in freelance page.

--- public/js/freelance.js
+++ public/js/freelance.js
@@ -3,7 +3,6 @@ function mainResize(){
 	let main = document.querySelector("main")
 	const height = `${window.innerHeight - headerHeight - 20}`
 	main.setAttribute("style", `height:${height}px;max-Height:${height}px`)
-	console.log(main, main.style.height)
 }
 window.addEventListener("resize", mainResize())
 mainResize()
